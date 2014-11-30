cb2ed7658e2d965c2a5bc70cd0cdc7b0b3681323
James <james@jarofgreen.co.uk>
2014-11-30 18:26:03 +0000

    Structure for core

---
+++ src/JMBTechnologyLimited/RRuleUnravel/RRuleUnraveling.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace JMBTechnologyLimited\RRuleUnravel;
+
+/**
+ *
+ * @link https://github.com/JMB-Technology-Limited/ParseDateTimeRangeString
+ * @license https://raw.github.com/JMB-Technology-Limited/RRuleUnravel/master/LICENSE.txt 3-clause BSD
+ * @copyright (c) 2013-2014, JMB Technology Limited, http://jmbtechnology.co.uk/
+ * @author James Baster <james@jarofgreen.co.uk>
+ */
+
+class RRuleUnraveling {
+
+	/** @var  RRule */
+	protected $rrule;
+
+	function __construct($rrule)
+	{
+		$this->rrule = $rrule;
+	}
+
+	/**
+	 * @return \JMBTechnologyLimited\RRuleUnravel\RRule
+	 */
+	public function getRrule()
+	{
+		return $this->rrule;
+	}
+
+
+
+
+}
+
--- src/JMBTechnologyLimited/RRuleUnravel/Unraveler.php
+++ src/JMBTechnologyLimited/RRuleUnravel/Unraveler.php
@@ -12,7 +12,34 @@ namespace JMBTechnologyLimited\RRuleUnravel;
 
 class Unraveler {
 
+	/** @var  RRuleUnraveling */
+	protected $rruleUnraveling;
+
+	/** @var  \DateTime */
+	protected $start;
+
+	/** @var  \DateTime */
+	protected $end;
+
+	protected $timezone;
+
+	function __construct(RRule $rrule, \DateTime $start, \DateTime $end, $timezone='UTC')
+	{
+		$this->rruleUnraveling = new RRuleUnraveling($rrule);
+		$this->start = $start;
+		$this->end = $end;
+		$this->timezone = $timezone;
+	}
 
 
+	public function process()
+	{
+
+	}
+
+	public function getResults()
+	{
+
+	}
 
 }
