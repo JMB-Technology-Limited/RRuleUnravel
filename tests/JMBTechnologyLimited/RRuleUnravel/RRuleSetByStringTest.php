bcdf637ca73401dee20a53fa4387a25e287e441b
James <james@jarofgreen.co.uk>
2014-11-30 18:29:03 +0000

    Correct docblocks

--- tests/JMBTechnologyLimited/RRuleUnravel/RRuleSetByStringTest.php
+++ tests/JMBTechnologyLimited/RRuleUnravel/RRuleSetByStringTest.php
@@ -5,9 +5,9 @@ namespace JMBTechnologyLimited\RRuleUnravel;
 
 /**
  *
- * @link https://github.com/JMB-Technology-Limited/ParseDateTimeRangeString
- * @license https://raw.github.com/JMB-Technology-Limited/ParseDateTimeRangeString/master/LICENSE.txt 3-clause BSD
- * @copyright (c) 2013-2014, JMB Technology Limited, http://jmbtechnology.co.uk/
+ * @link https://github.com/JMB-Technology-Limited/RRuleUnravel
+ * @license https://raw.github.com/JMB-Technology-Limited/RRuleUnravel/master/LICENSE.txt 3-clause BSD
+ * @copyright (c) 2014, JMB Technology Limited, http://jmbtechnology.co.uk/
  * @author James Baster <james@jarofgreen.co.uk>
  */
 class RRuleSetByStringTest extends \PHPUnit_Framework_TestCase {
