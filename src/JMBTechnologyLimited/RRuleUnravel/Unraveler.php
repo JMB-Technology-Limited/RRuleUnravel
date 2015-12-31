ed2988809649452d695f9398d49aced5a0a277fa
James <james@jarofgreen.co.uk>
2015-12-31 20:25:08 +0000

    Update year in license and remove year from files. (You don't legally need it and it causes hassle updating.)

--- src/JMBTechnologyLimited/RRuleUnravel/Unraveler.php
+++ src/JMBTechnologyLimited/RRuleUnravel/Unraveler.php
@@ -10,7 +10,7 @@ use JMBTechnologyLimited\RRuleUnravel\internal\ICalDataUnravelling;
  *
  * @link https://github.com/JMB-Technology-Limited/RRuleUnravel
  * @license https://raw.github.com/JMB-Technology-Limited/RRuleUnravel/master/LICENSE.txt 3-clause BSD
- * @copyright (c) 2014, JMB Technology Limited, http://jmbtechnology.co.uk/
+ * @copyright (c) JMB Technology Limited, http://jmbtechnology.co.uk/
  * @author James Baster <james@jarofgreen.co.uk>
  */
 
--- src/JMBTechnologyLimited/RRuleUnravel/internal/MonthCalendarForDaysOfWeek.php
+++ src/JMBTechnologyLimited/RRuleUnravel/internal/MonthCalendarForDaysOfWeek.php
@@ -7,7 +7,7 @@ namespace JMBTechnologyLimited\RRuleUnravel\internal;
  *
  * @link https://github.com/JMB-Technology-Limited/RRuleUnravel
  * @license https://raw.github.com/JMB-Technology-Limited/RRuleUnravel/master/LICENSE.txt 3-clause BSD
- * @copyright (c) 2014, JMB Technology Limited, http://jmbtechnology.co.uk/
+ * @copyright (c) JMB Technology Limited, http://jmbtechnology.co.uk/
  * @author James Baster <james@jarofgreen.co.uk>
  */
 
